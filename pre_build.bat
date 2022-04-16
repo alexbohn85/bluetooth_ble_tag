1d40ad1db0817d7ee8fd52aa9e227e5b577dfde0
Alex_Bohn <alex.bohn@guardrfid.com>
2022-04-16 15:41:16 -0700

    Fix issue with pre_build.bat to handle double quotes marks in git commit message

--- pre_build.bat
+++ pre_build.bat
@@ -8,12 +8,6 @@ echo Running pre build script...
 :: globals
 set GIT_FOLDER=.git\
 set OUTPUT_FILE=git_log.h
-set GIT_URL_TEMP=""
-
-:: output file structure
-set H_HEADER=//! =============== Auto-generated file, do not edit. ===============
-::set H_BODY="#ifndef GIT_INFO_PRESENT%%n#define GIT_INFO_PRESENT%%n%%nstatic const char* git_line_1 = \"%%h (%%D) by %%aE %%as %%ah\";%%nstatic const char* git_line_2 = %%<(80,trunc)\"%%s\";"
-set H_BODY="#ifndef GIT_INFO_PRESENT%%n#define GIT_INFO_PRESENT%%n%%n#define GIT_LINE_1 \"%%h (%%D) by %%aE %%as %%ah\"%%n#define GIT_LINE_2 %%<(80,trunc)\"%%s\""
 
 :: workaround to get to root folder
 if NOT exist %GIT_FOLDER% (
@@ -29,29 +23,33 @@ if exist %OUTPUT_FILE% (
     rm %OUTPUT_FILE%
 )
 
-:: get repo url (if not set it will be "")
-FOR /F "tokens=*" %%g IN ('git config --get remote.origin.url') DO SET GIT_URL_TEMP=%%g
+:: gather git log data
+FOR /F "tokens=*" %%g IN ('git config --get remote.origin.url') DO SET GIT_URL=%%g
+FOR /F "tokens=*" %%g IN ('git log -1 --pretty^=\"%%h\"') DO SET GIT_COMMIT_ID=%%g
+FOR /F "tokens=*" %%g IN ('git log -1 --pretty^=\"%%D\"') DO SET GIT_BRANCH=%%g
+FOR /F "tokens=*" %%g IN ('git log -1 --pretty^=\"%%as\"') DO SET AUTHOR_EMAIL=%%g
+FOR /F "tokens=*" %%g IN ('git log -1 --pretty^=\"%%ah\"') DO SET GIT_COMMIT_DATETIME=%%g
+FOR /F "tokens=*" %%g IN ('git log -1 --pretty^=\"%%<(100,trunc)%%s\"') DO SET GIT_COMMIT_MSG=%%g
 
-:: generate output file
-echo Generating %C_HEADER_OUTPUT_FILE% ...
+:: remove any double quotes from git message (if exists)
+set GIT_COMMIT_MSG="%GIT_COMMIT_MSG:"=%"
 
+:: build file structure
+set H_HEADER=//! =============== Auto-generated file, do not edit. ===============
+set GIT_LINE_1=#define GIT_LINE_1 "%GIT_COMMIT_ID:"=% (%GIT_BRANCH:"=%) %AUTHOR_EMAIL:"=% %GIT_COMMIT_DATETIME:"=%"
+set GIT_LINE_2=#define GIT_LINE_2 "%GIT_COMMIT_MSG:"=%"
+set GIT_URL=#define GIT_URL "%GIT_URL:"=%"
+
+echo Generating %OUTPUT_FILE% ...
+
+:: write output file
 echo %H_HEADER% >> %OUTPUT_FILE%
 echo, >> %OUTPUT_FILE%
-git log -1 --decorate=full --decorate-refs-exclude='refs/heads' --pretty=%H_BODY% >> %OUTPUT_FILE%
-echo #define GIT_URL "%GIT_URL_TEMP%" >> %OUTPUT_FILE%
-echo #endif >> %OUTPUT_FILE%
-echo, >> %OUTPUT_FILE%
+echo %GIT_LINE_1% >> %OUTPUT_FILE%
+echo %GIT_LINE_2% >> %OUTPUT_FILE%
+echo %GIT_URL% >> %OUTPUT_FILE%
 
-echo pre_build... All Done!
+echo All Done!
 
 :: exit
 goto :eof
-
-
-:: backup stuff
-::set C_HEADER_TEMPLATE="#ifndef GIT_INFO_PRESENT%%n#define GIT_INFO_PRESENT%%nstatic const char* git_info_commit_id = \"%%C(bold green)%%h %%C(reset)%%C(bold)%%D %%C(reset) Author: %%aN %%ce, %%ch [%%ar]\";%%nstatic const char* git_commit_comment = %%<(80,trunc)\"%%s\";"
-::git log --decorate=full --decorate-refs-exclude='refs/heads' --pretty=format:'%C(bold green)%h %C(reset)%C(bold)%D %C(reset) Author: %aN %ce, %ch [%ar]%n commit msg: %s'
-::get system date and time
-::set COMPILATION_TIME=%DATE%%TIME%
-::set COMPILATION_TIME=%COMPILATION_TIME:~0,-3%
-::set GIT_URL_TEMP=""
